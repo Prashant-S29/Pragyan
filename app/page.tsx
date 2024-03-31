ca12185e69ca54a270f3b199fd25d47bbd942cc8
Prashant-S29 <prashant.s2922@gmail.com>
2024-03-31 19:44:41 +0530

    Initial Commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const HOME = () => {
+  return (
+    <>
+      <div>
+        <span>Welcome to Pragyan</span>
+      </div>
+    </>
+  );
+};
+
+export default HOME;
